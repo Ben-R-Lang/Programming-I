688893f9bcc3b5a1646c8edc092a6d24b674ccdb
Benjamin Lang <112718639+Ben-R-Lang@users.noreply.github.com>
2023-01-26 15:05:43 +0000

    .

---
+++ lang52a.py
@@ -0,0 +1,9 @@
+# + - * /
+length = input("Enter length: ")
+length = int(length)
+width = input("Enter width: ")
+width = int(width)
+area = length * width
+perim = 2 * length + 2 * width
+print("Area:", area)
+print("Perim:", perim)
---
+++ lang54b.py
@@ -0,0 +1,12 @@
+# Input 4 numbers
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+num3 = int(input("Enter num3: "))
+num4 = int(input("Enter num4: "))
+
+# Calc sum and average
+sum = num1 + num2 + num3 + num4
+ave = sum / 4
+# Print sum and average
+print("Sum:", sum)
+print("Average:", ave)
